5f9a4375dd74efb7f1082d244d5c6d01871bda78
Amy Ha <>
2021-12-19 13:12:09 +0000

    Moved Int extension to its own file

--- RewireStroke/ViewControllers/StartWorkoutViewController/StartWorkoutViewController.swift
+++ RewireStroke/ViewControllers/StartWorkoutViewController/StartWorkoutViewController.swift
@@ -169,17 +169,3 @@ protocol ExerciseSelectionDelegate {
     func reloadProgress(at: Int)
 }
 
-// Taken from: https://stackoverflow.com/questions/26794703/swift-integer-conversion-to-hours-minutes-seconds
-extension Int {
-
-    var timeFormat: String {
-
-        let (m,s) = ((self % 3600) / 60, (self % 3600) % 60)
-
-        let m_string =  m < 10 ? "0\(m)" : "\(m)"
-        let s_string =  s < 10 ? "0\(s)" : "\(s)"
-
-        return "\(m_string):\(s_string)"
-    }
-}
-
