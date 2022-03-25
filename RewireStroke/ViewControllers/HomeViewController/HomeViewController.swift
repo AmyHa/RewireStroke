8c685fbc89dcb4cb46dc49815274535b4f6b664d
Amy Ha <>
2022-03-25 08:15:57 +0000

    Added floating action button to homepage

--- RewireStroke/ViewControllers/HomeViewController/HomeViewController.swift
+++ RewireStroke/ViewControllers/HomeViewController/HomeViewController.swift
@@ -16,6 +16,21 @@ class HomeViewController: UIViewController {
     
     private var host: UIHostingController<HomeScrollView>!
     
+    private let floatingButton: UIButton = {
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 60, height: 60))
+        button.layer.cornerRadius = 30
+        button.layer.shadowRadius = 10
+        button.layer.shadowOpacity = 0.3
+        button.backgroundColor = Colours.primaryDark
+        
+        let image = UIImage(systemName: "plus",
+                            withConfiguration: UIImage.SymbolConfiguration(pointSize: 32, weight: .medium))
+        button.setImage(image, for: .normal)
+        button.tintColor = .white
+        button.setTitleColor(.white, for: .normal)
+        return button
+    }()
+    
     convenience init() {
         self.init(nibName: nil, bundle: nil)
     }
@@ -34,5 +49,16 @@ class HomeViewController: UIViewController {
         hostView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         hostView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0).isActive = true
         hostView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0).isActive = true
+        
+        view.addSubview(floatingButton)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        floatingButton.frame = CGRect(
+            x: view.frame.size.width - 70,
+            y: view.frame.size.height - 160,
+            width: 60,
+            height: 60)
     }
 }
