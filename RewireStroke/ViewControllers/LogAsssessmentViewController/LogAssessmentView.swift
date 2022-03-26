951a712a726fef693d074f6a7db2dc0d66d7033f
Amy Ha <>
2022-03-26 17:59:00 +0000

    Added mood, fatigue and pain scale views

--- RewireStroke/ViewControllers/LogAsssessmentViewController/LogAssessmentView.swift
+++ RewireStroke/ViewControllers/LogAsssessmentViewController/LogAssessmentView.swift
@@ -33,17 +33,20 @@ struct LogAssessmentView: View {
     
     var body: some View {
         
+        let moodScaleView = ScaleView(icons: ["iconHappyFace", "iconOkFace", "iconSadFace", "iconLowFace"], labels: ["I'm happy", "I feel okay", "I feel sad", "I feel really low"], navTitle: "How do you feel?")
+        let painScaleView = ScaleView(icons: ["iconPainFree", "iconPainMinor", "iconPainMajor", "iconPainSevere"], labels: ["I'm painfree", "I have some minor aches", "Pain is impacting on \nmy daily function", "I am in severe pain"], navTitle: "Are you in pain?")
+        let fatigueScaleView = ScaleView(icons: ["iconBatteryFull", "iconBatteryThreeQuarters", "iconBatteryHalf", "iconBatteryLow"], labels: ["I'm full of beans!", "I'm able to \ncomplete my daily activities", "I feel tired", "I have very little energy"], navTitle: "How much energy do you have?")
         NavigationView{
             VStack {
-                LogView(icon: "", text: "Log your mood", colour: Colours.moodColor)
+                LogView(icon: "iconHappyFace", text: "Log your mood", colour: Colours.moodColor, scaleView: moodScaleView)
                 Spacer().frame(height: 20)
-                LogView(icon: "", text: "Log your energy levels", colour: Colours.fatigueColor)
+                LogView(icon: "iconBattery", text: "Log your energy levels", colour: Colours.fatigueColor, scaleView: fatigueScaleView)
                 Spacer().frame(height: 20)
-                LogView(icon: "", text: "Log your pain levels", colour: Colours.painColor)
+                LogView(icon: "iconPainCircles", text: "Log your pain levels", colour: Colours.painColor, scaleView: painScaleView)
                 
             }.navigationTitle(Text("How are you feeling?"))
                 .offset(y: -90)
-        }.frame(minHeight: minRowHeight * 18).font(Font.outfitBold(size: 20.0)).foregroundColor(.black)
+        }
     }
 }
 
@@ -52,7 +55,8 @@ struct LogView: View {
     var icon: String
     var text: String
     var colour: Color
-//    var activityViewModel: ActivityViewModel
+    var scaleView: ScaleView
+
     @State private var isWorkoutDisplayed = false
     
     var body: some View {
@@ -62,36 +66,112 @@ struct LogView: View {
                 Image(icon)
                     .resizable()
                     .scaledToFit()
-                    .clipped().frame(width: 40, height: 60)
+                    .clipped().frame(width: 60, height: 40)
                 Text(String(text)).font(.outfitRegular(size: 25)).foregroundColor(colour)
             }.frame(width: 300)
             
         }
-//
-//        .onTapGesture {
-//            isWorkoutDisplayed = true
-//        }
-//        .sheet(isPresented: $isWorkoutDisplayed, onDismiss: {
-//            print("the new workouts are... \(activityViewModel.LLworkouts.value)")
-//        }, content: {
-//            WorkoutPageView(workout: activityViewModel.LLworkouts.value[0])
-//        })
-    }
-    
-    struct WorkoutPageView: UIViewControllerRepresentable {
-        @Environment(\.presentationMode) var presentationMode
-
-        var workout: Workout
-
-        func makeUIViewController(context: Context) -> WorkoutViewController {
-            return WorkoutViewController(workout: workout)
+        .onTapGesture {
+            isWorkoutDisplayed = true
         }
-
-        func updateUIViewController(_ uiViewController: WorkoutViewController, context: Context) {
-
-        }
-
-        typealias UIViewControllerType = WorkoutViewController
-
+        .sheet(isPresented: $isWorkoutDisplayed, onDismiss: {
+            
+        }, content: {
+            scaleView
+        })
     }
 }
+
+struct ScaleView: View {
+    
+    @Environment(\.presentationMode) var presentationMode
+    
+    var icons: [String]
+    var labels: [String]
+    var navTitle: String
+    
+    @State var maxHeight: CGFloat = 320
+    @State var sliderProgress: CGFloat = 0
+    @State var sliderHeight: CGFloat = 0
+    @State var lastDragValue: CGFloat = 0
+    
+    var body: some View {
+        NavigationView {
+            
+            VStack {
+                HStack{
+                    VStack{
+                        HStack{
+                            Image(icons[0])
+                                .resizable()
+                                .scaledToFit()
+                                .clipped().frame(width: 70, height: 40)
+                            Text(String(labels[0])).font(.outfitRegular(size: 18))
+                        }.frame(width: 260, alignment: .leading)
+                        Spacer().frame(height: 50)
+                        HStack{
+                            Image(icons[1])
+                                .resizable()
+                                .scaledToFit()
+                                .clipped().frame(width: 70, height: 40)
+                            Text(String(labels[1])).font(.outfitRegular(size: 18))
+                        }.frame(width: 260, alignment: .leading)
+                        Spacer().frame(height: 50)
+                        HStack{
+                            Image(icons[2])
+                                .resizable()
+                                .scaledToFit()
+                                .clipped().frame(width: 70, height: 40)
+                            Text(String(labels[2])).font(.outfitRegular(size: 18))
+                        }.frame(width: 260, alignment: .leading)
+                        Spacer().frame(height: 50)
+                        HStack{
+                            Image(icons[3])
+                                .resizable()
+                                .scaledToFit()
+                                .clipped().frame(width: 70, height: 40)
+                            Text(String(labels[3])).font(.outfitRegular(size: 18))
+                        }.frame(width: 260, alignment: .leading)
+                    }.foregroundColor(Colours.primaryDarkColor)
+                    ZStack(alignment: .bottom, content: {
+                        
+                        RoundedRectangle(cornerRadius: 20, style: .continuous).fill(Colours.grey02Color)
+                        RoundedRectangle(cornerRadius: 20, style: .continuous).fill(Colours.primaryBlueColor).frame(height: sliderHeight)
+                    })
+                    .frame(width: 20, height: maxHeight)
+                        .gesture(DragGesture(minimumDistance: 0).onChanged({ value in
+                            
+                            // getting drag value
+                            let translation = value.translation
+                            sliderHeight = -translation.height + lastDragValue
+                            
+                            // limiting slider height value
+                            sliderHeight = sliderHeight > maxHeight ? maxHeight : sliderHeight
+                            sliderHeight = sliderHeight >= 0 ? sliderHeight : 0
+                        }).onEnded({ value in
+                            // storing last drag value
+                            lastDragValue = sliderHeight
+                        })
+                        )
+                    
+                }
+                Spacer().frame(height: 50)
+                Button {
+                    presentationMode.wrappedValue.dismiss()
+                } label: {
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(Colours.primaryBlueColor)
+                            .frame(width: 110, height: 45)
+                        Text("Submit").font(.outfitSemiBold(size: 15))
+                            .padding()
+                            .foregroundColor(.white)
+                    }
+                    
+                }
+            }.navigationTitle(navTitle)
+            
+        }
+    }
+}
+
--- RewireStroke/ViewControllers/LogAsssessmentViewController/LogAssessmentViewController.swift
+++ RewireStroke/ViewControllers/LogAsssessmentViewController/LogAssessmentViewController.swift
@@ -7,24 +7,32 @@
 //
 
 import UIKit
+import SwiftUI
 
 class LogAssessmentViewController: UIViewController {
-
+    
+    @IBOutlet weak var containerView: UIView!
+    
+    private var host: UIHostingController<LogAssessmentView>!
+    
+    convenience init() {
+        self.init(nibName: nil, bundle: nil)
+    }
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        let logAssessmentView = LogAssessmentView()
+        host = UIHostingController(rootView: logAssessmentView)
+        
+        guard let hostView = host.view else { return }
+        
+        containerView.addSubview(hostView)
+        
+        // Add constraints
+        hostView.translatesAutoresizingMaskIntoConstraints = false
+        hostView.centerYAnchor.constraint(equalTo: containerView.centerYAnchor).isActive = true
+        hostView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        hostView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0).isActive = true
+        hostView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0).isActive = true
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
