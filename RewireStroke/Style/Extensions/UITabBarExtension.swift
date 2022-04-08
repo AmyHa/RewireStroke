b0ebb7d34fb3d5ca1825e5a564d6bc09d292ccc1
Amy Ha <>
2022-04-08 11:05:35 +0100

    new tab icon assets (excluding Info and Profile for now)

---
+++ RewireStroke/Style/Extensions/UITabBarExtension.swift
@@ -0,0 +1,9 @@
+//
+//  UITabBarExtension.swift
+//  RewireStroke
+//
+//  Created by Amy Ha on 30/03/2022.
+//  Copyright © 2022 Amy Ha. All rights reserved.
+//
+
+import Foundation
---
+++ RewireStroke/Style/SubClasses/CustomTabBarController.swift
@@ -0,0 +1,9 @@
+//
+//  CustomTabBarController.swift
+//  RewireStroke
+//
+//  Created by Amy Ha on 08/04/2022.
+//  Copyright © 2022 Amy Ha. All rights reserved.
+//
+
+import Foundation
